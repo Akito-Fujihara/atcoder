88980aa742d5968b637b06f22463ec968e841ad2
藤原彰人 <1998922akito@gmail.com>
2023-08-14 20:15:46 +0900

    ダーツ

---
+++ ダーツ/main.py
@@ -0,0 +1,22 @@
+from itertools import product
+from bisect import bisect
+
+N, M = (int(x) for x in input().split())
+
+numbers = [0]
+
+for _ in range(N):
+    numbers.append(int(input()))
+
+p = set()
+for i, j in product(range(N + 1), repeat=2):
+    p.add(numbers[i] + numbers[j])
+p = list(sorted(p))
+ans = 0
+for i in p:
+    index = bisect(p, M - i)
+    if index == 0:
+        continue
+    ans = max(ans, i + p[index - 1])
+
+print(ans)
